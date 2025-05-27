8ca52387c134ccaaf736e77734427734b2407b6e
churchilldotexe <churchillexe@gmail.com>
2025-05-27 21:23:51 +0800

    fix: lint error

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <button class="btn btn-secondary">
     Secondary
     <Icon name="tabler:map" />
