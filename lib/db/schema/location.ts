74aebe90b7915da14219a66ab916e99f2d8cc58c
Churchill Jay Sungcados <163730939+churchilldotexe@users.noreply.github.com>
2025-06-03 21:30:22 +0800

    feat: auth with better auth, added schema and migrated (#54)

--- lib/db/schema/location.ts
+++ lib/db/schema/location.ts
@@ -1,5 +1,7 @@
 import { int, integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
+
 export const location = sqliteTable("location", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
@@ -7,6 +9,7 @@ export const location = sqliteTable("location", {
   description: text(),
   lat: real().notNull(),
   long: real().notNull(),
+  userId: int().references(() => user.id),
   createdAt: integer().$default(() => Date.now()),
   updatedAt: integer().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
