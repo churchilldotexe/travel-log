913c2f4fcceaad9fae7cac360e956adaa0951ebb
Churchill Jay Sungcados <163730939+churchilldotexe@users.noreply.github.com>
2025-06-05 21:02:29 +0800

    feat: logout button and user profile when logged in (#56)

--- components/auth-btn.vue
+++ components/auth-btn.vue
@@ -5,7 +5,25 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.isLoading" class="btn btn-accent" @click="authStore.signIn">
+  <div v-if="authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn m-1 space-x-2">
+      <div v-if="authStore.user.image" class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out" class="gap-x-2">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+  <button v-else :disabled="authStore.isLoading" class="btn btn-accent" @click="authStore.signIn">
     Sign in with Github
 
     <span v-if="authStore.isLoading" class="loading loading-spinner loading-md" />
---
+++ pages/sign-out.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useAuthStore } from "~/store/auth";
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <section class="container m-auto min-h-96 flex justify-center items-center flex-col gap-4">
+    <span class="loading loading-spinner w-12" />
+  </section>
+</template>
