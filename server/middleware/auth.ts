276db385fc14f8d0e563aa700205a200dbd3cff5
Churchill Jay Sungcados <163730939+churchilldotexe@users.noreply.github.com>
2025-06-09 19:57:20 +0800

    feat: middleware setup ,added gates to dashboard redirect user unauth user to home (#57)

---
+++ server/middleware/auth.ts
@@ -0,0 +1,11 @@
+import { auth } from "~/lib/auth";
+
+export default defineEventHandler(async (event) => {
+  const session = await auth.api.getSession({
+    headers: event.headers,
+  });
+
+  if (event.path.startsWith("/dashboard") && !session) {
+    await sendRedirect(event, "/", 302);
+  }
+});
