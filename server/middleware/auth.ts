b9e1daeece4ffa316e8173aabe2c8107dc7e1409
churchilldotexe <churchillexe@gmail.com>
2025-06-09 20:39:02 +0800

    chore: ssr for session

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,11 +1,12 @@
 import { auth } from "~/lib/auth";
 
 export default defineEventHandler(async (event) => {
-  const session = await auth.api.getSession({
-    headers: event.headers,
-  });
-
-  if (event.path.startsWith("/dashboard") && !session) {
-    await sendRedirect(event, "/", 302);
+  if (event.path.startsWith("/dashboard")) {
+    const session = await auth.api.getSession({
+      headers: event.headers,
+    });
+    if (!session?.user) {
+      await sendRedirect(event, "/", 302);
+    }
   }
 });
